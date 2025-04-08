950cb3dc414c2532499273615a1bc6b18d5169a8
VIDIT ASATI <viditasati@MacBook-Air-3.local>
2025-04-08 11:22:52 +0530

    Learned about datatyps

---
+++ _datatypes.js
@@ -0,0 +1,22 @@
+"use strict"  // treat all js code as newer version
+
+console.log(3+3);
+console.log("vidit");  // code readability should be high
+
+let name="vidit"
+let age=21
+let isloggedIn=false
+
+
+// number=> 2 to power 53
+// bigint 
+// string =>""
+// boolean => true / false
+// null=> standalone value
+// undefined=> not defined
+// symbol => unique
+
+// type of (we can know type of anything by this)
+
+console.log(typeof undefined)
+console.log(typeof null)
--- test.js
+++ test.js
@@ -1,2 +1,20 @@
 console.log("Vidit");
 
+const accid=144553;
+let accemail="vid@gmail.com";
+let acccity="jaipur";
+let accstate;
+accemail="hc@sid.com";
+// accid=2;  not allowed
+
+acccity="sihora";
+
+console.table([accid,accemail,acccity,accstate]);
+
+
+/*
+
+always prefer to use let and const dont use var
+
+*/
+
